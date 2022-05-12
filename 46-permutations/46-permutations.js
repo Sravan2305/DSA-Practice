874aea1a1585a52864205c09b1a68061c7c0f23b
Sravan Kumar <48352774+Sravan2305@users.noreply.github.com>
2022-05-12 21:26:05 +0530

    Time: 95 ms (54.55%), Space: 44.7 MB (85.77%) - LeetHub

---
+++ 46-permutations/46-permutations.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function(nums) {
+    const res = []
+    function helper(ind , acc){  
+        if( ind >= nums.length ){
+            res.push( [...acc] )
+        }
+        for( let i=ind; i<nums.length; i++ ){
+            swap(i ,ind ,acc )
+            helper( ind+1 , [...acc] )
+            swap(i ,ind ,acc )
+
+        }
+        
+    }
+    helper(0 , nums)
+    return res
+};
+
+function swap( i , j , arr ){
+    let t = arr[i]
+    arr[i] = arr[j]
+    arr[j] = t 
+}
