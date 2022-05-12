4ad9740e94a23b2e25bc706ec2cf4bf44db0a717
Sravan Kumar <48352774+Sravan2305@users.noreply.github.com>
2022-05-12 22:36:46 +0530

    Time: 110 ms (34.37%), Space: 44.5 MB (89.72%) - LeetHub

--- 46-permutations/46-permutations.js
+++ 46-permutations/46-permutations.js
@@ -6,15 +6,13 @@ var permute = function(nums) {
     const res = []
     function helper(ind , acc){  
         if( ind >= nums.length ){
-            res.push( [...acc] )
+           return  res.push( [...acc] )
         }
         for( let i=ind; i<nums.length; i++ ){
             swap(i ,ind ,acc )
             helper( ind+1 , [...acc] )
             swap(i ,ind ,acc )
-
         }
-        
     }
     helper(0 , nums)
     return res
