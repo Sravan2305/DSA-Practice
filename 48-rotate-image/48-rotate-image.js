22106c6ff82d7ae558d3a06cf4e97eae29fe8e47
Sravan Kumar <48352774+Sravan2305@users.noreply.github.com>
2022-05-18 17:31:25 +0530

    Time: 127 ms (5.10%), Space: 46.4 MB (5.06%) - LeetHub

---
+++ 48-rotate-image/48-rotate-image.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate = function(matrix) {
+    let rows = matrix.length , cols = matrix[0].length
+    for( let i=0; i<Math.floor(rows/2) ; i++ )
+        for(let j = 0 ; j<cols; j++){
+            let temp = matrix[i][j]
+            matrix[i][j] = matrix[rows-i-1][j]
+            matrix[rows-i-1][j] = temp 
+        }
+        for( let i=0; i<rows ; i++ )
+        for(let j = 0 ; j< i ; j++){
+            let temp = matrix[i][j]
+            matrix[i][j] = matrix[j][i]
+            matrix[j][i] = temp 
+        }
+    console.log(matrix)
+};
