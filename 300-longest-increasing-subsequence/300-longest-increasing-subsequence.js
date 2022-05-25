d8f6fe7b46b7f506465609a3dfe9eb87f09987a0
Sravan Kumar <48352774+Sravan2305@users.noreply.github.com>
2022-05-25 20:32:29 +0530

    Time: 298 ms (17.34%), Space: 45.5 MB (8.72%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.js
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.js
@@ -2,18 +2,31 @@
  * @param {number[]} nums
  * @return {number}
  */
-var lengthOfLIS = function(nums) {
-    const dp = new Array(nums.length).fill(0)
-    let globalMax = 0
-    for( let i = 0 ; i<nums.length; i++ ){
-        let max = 0 
-        for( let j = 0; j<i ; j++ ){
-            if( nums[j] < nums[i] )
-            max = Math.max( max , dp[j] )
+var lengthOfLIS = function(arr) {
+//     const dp = new Array(nums.length).fill(0)
+//     let globalMax = 0
+//     for( let i = 0 ; i<nums.length; i++ ){
+//         let max = 0 
+//         for( let j = 0; j<i ; j++ ){
+//             if( nums[j] < nums[i] )
+//             max = Math.max( max , dp[j] )
+//         }
+//         dp[i] = max+1
+//         globalMax = Math.max(globalMax , dp[i])
+//     }
+//     return globalMax
+    
+        let dp = new Array(arr.length).fill(0) , max = 0
+    for(let i = 0; i<arr.length; i++){
+        let res = 0
+        for(let j = 0; j<i; j++){
+            if(arr[j]<arr[i]){
+                res = Math.max(res , dp[j])
+            }
         }
-        dp[i] = max+1
-        globalMax = Math.max(globalMax , dp[i])
+        dp[i] = res+1
+        max = Math.max(max ,dp[i])
     }
-    // console.log(dp)
-    return globalMax
+    console.log(dp)
+    return max
 };
