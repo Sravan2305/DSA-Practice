9ce6b07efb5cd4d5f2ddfe6627d98640ab23eaf1
Sravan Kumar <48352774+Sravan2305@users.noreply.github.com>
2022-05-29 07:57:32 +0530

    Time: 112 ms (74.45%), Space: 53.1 MB (70.25%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.js
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.js
@@ -5,7 +5,8 @@
  */
 var longestCommonSubsequence = function(text1, text2) {
     const memo = {}
-    return topDown(text1 , text2 , 0 , 0 , memo)    
+    // return topDown(text1 , text2 , 0 , 0 , memo)    
+    return bottomUp(text1 , text2)
 };
 
 function topDown(s1 , s2 , i , j, memo){
@@ -24,4 +25,15 @@ function topDown(s1 , s2 , i , j, memo){
     memo[arg] = Math.max(topDown(s1,s2,i+1,j,memo) ,topDown(s1,s2,i,j+1,memo))
     
     return memo[arg]
+}
+
+function bottomUp(s1 , s2){
+        let dp = new Array(s1.length+1).fill(0).map(x=> new Array(s2.length+1).fill(0))
+    for(let i = 1 ; i<=s1.length; i++){
+        for(let j =1 ; j<=s2.length; j++){
+            if(s1[i-1] == s2[j-1]) dp[i][j] = dp[i-1][j-1]+1
+            else dp[i][j] = Math.max( dp[i][j-1], dp[i-1][j] )
+        }
+    }
+    return dp[s1.length][s2.length]
 }
