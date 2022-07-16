c03cbed7ff8f4953819d3b6948ffe49107f598fb
Sravan Kumar <48352774+Sravan2305@users.noreply.github.com>
2022-07-16 20:41:49 +0530

    Time: 73 ms (82.03%), Space: 44.5 MB (31.49%) - LeetHub

---
+++ 107-binary-tree-level-order-traversal-ii/107-binary-tree-level-order-traversal-ii.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrderBottom = function(root) {
+    let res = []
+    helper( root , 0 )
+    return res.reverse()
+    
+    function helper( node , level ){
+        if( !node ) return
+        if( !res[level] ) res[level] = [ node.val ]
+        else res[level].push( node.val )
+        helper( node.left , level+1 )
+        helper( node.right, level+1 )
+    }
+};
